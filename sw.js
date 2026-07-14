02b6632af1cbdc94bfe469b97a6786b14b36a179
ducatielias <yoigo07082025@gmail.com>
2026-07-14 19:44:56 +0200

    v1.04

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * MODIFICADO: Soporte para mensaje "getVersion" que devuelve CACHE_VERSION
  */
 
-const CACHE_VERSION = 'gym-notes-v1-03';
+const CACHE_VERSION = 'gym-notes-v1-04';
 const CACHE_NAME = CACHE_VERSION;
 
 // ============================================================
